e770222fd598352732ea38c3d8bdaa89766231d1
Martin Smith <msmith@hiveit.co.uk>
2016-06-02 09:56:03 +0100

    Fixing routing on v2

--- app/routes.js
+++ app/routes.js
@@ -6,9 +6,9 @@ router.get('/', function (req, res) {
 });
 
 
-// Branchin
+// Branching
 
-
+// V1
 router.get('/outgoing-comms/1/3/', function (req, res) {
 
   // get the answer from the query string (eg. ?over18=false)
@@ -71,6 +71,69 @@ router.get('/outgoing-comms/1/', function (req, res) {
   res.render('/outgoing-comms/1/1', {'errors_on' : req.query.errors});
 });
 
+
+// V2
+router.get('/outgoing-comms/2/3/', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var hasDependants = req.query.radioHasDependant;
+  var radioDependant1 = req.query.radioDependant1;
+  var radioDependant2 = req.query.radioDependant2;
+  var radioPolice = req.query.radioPolice;
+
+  if (hasDependants == "no" & radioPolice == "no"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/2/3-approval-nodependants-nopolice");
+  }
+
+  else  if (hasDependants == "no" & radioPolice == "yes"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/2/3-approval-nodependants-withpolice");
+  }
+
+  else if (hasDependants == "yes" & radioPolice == "no"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/2/3-approval-withdependants-nopolice");
+  }
+
+  else if (hasDependants == "yes" & radioPolice == "yes"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/2/3-approval-withdependants-withpolice");
+  }
+
+
+  else {
+
+    // if over18 is any other value (or is missing) render the page requested
+   // res.render('examples/over-18');
+
+  }
+
+});
+
+
+router.get('/outgoing-comms/2/2a/', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var visa = req.query.radioVisa;
+
+  if (visa == "refuse"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/2/placeholder");
+  }
+  else if (visa == "approve"){
+     res.redirect("/outgoing-comms/2/2");
+  }
+
+  else {
+    res.redirect('/outgoing-comms/2/1?errors=on');
+  }
+
+});
+
+router.get('/outgoing-comms/2/', function (req, res) {
+  res.render('/outgoing-comms/2/1', {'errors_on' : req.query.errors});
+});
 
 
 module.exports = router;
