a45638ad8baac67783b2deb781093444d1b56cf2
Martin Smith <msmith@hiveit.co.uk>
2016-06-06 14:56:28 +0100

    Correcting linkcs and routes after creating v3 Creating PNN search and results page

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ router.get('/', function (req, res) {
 
 // Branching
 
-// V1
+// -------------------------------------------------------------- V1
 router.get('/outgoing-comms/1/3/', function (req, res) {
 
   // get the answer from the query string (eg. ?over18=false)
@@ -72,7 +72,7 @@ router.get('/outgoing-comms/1/', function (req, res) {
 });
 
 
-// V2
+// -------------------------------------------------------------- V2
 router.get('/outgoing-comms/2/3/', function (req, res) {
 
   // get the answer from the query string (eg. ?over18=false)
@@ -135,5 +135,70 @@ router.get('/outgoing-comms/2/', function (req, res) {
   res.render('/outgoing-comms/2/1', {'errors_on' : req.query.errors});
 });
 
+
+// -------------------------------------------------------------- V3
+
+
+router.get('/outgoing-comms/3/3/', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var hasDependants = req.query.radioHasDependant;
+  var radioDependant1 = req.query.radioDependant1;
+  var radioDependant2 = req.query.radioDependant2;
+  var radioPolice = req.query.radioPolice;
+
+  if (hasDependants == "no" & radioPolice == "no"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/3/3-approval-nodependants-nopolice");
+  }
+
+  else  if (hasDependants == "no" & radioPolice == "yes"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/3/3-approval-nodependants-withpolice");
+  }
+
+  else if (hasDependants == "yes" & radioPolice == "no"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/3/3-approval-withdependants-nopolice");
+  }
+
+  else if (hasDependants == "yes" & radioPolice == "yes"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/3/3-approval-withdependants-withpolice");
+  }
+
+
+  else {
+
+    // if over18 is any other value (or is missing) render the page requested
+   // res.render('examples/over-18');
+
+  }
+
+});
+
+
+router.get('/outgoing-comms/3/2a/', function (req, res) {
+
+  // get the answer from the query string (eg. ?over18=false)
+  var visa = req.query.radioVisa;
+
+  if (visa == "refuse"){
+    // redirect to the relevant page
+    res.redirect("/outgoing-comms/3/placeholder");
+  }
+  else if (visa == "approve"){
+     res.redirect("/outgoing-comms/3/2");
+  }
+
+  else {
+    res.redirect('/outgoing-comms/3/1?errors=on');
+  }
+
+});
+
+router.get('/outgoing-comms/3/', function (req, res) {
+  res.render('/outgoing-comms/3/1', {'errors_on' : req.query.errors});
+});
 
 module.exports = router;
